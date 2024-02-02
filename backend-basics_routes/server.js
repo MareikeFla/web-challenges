2456e4f24b76b9f9b2f3d1892873a8526344ceba
MareikeFla <mwidderich@gmail.com>
2024-02-02 12:31:38 +0100

    solved

--- backend-basics_routes/server.js
+++ backend-basics_routes/server.js
@@ -1,6 +1,11 @@
 import { createServer } from "node:http";
 
 export const server = createServer((request, response) => {
-  console.log("Test");
-  response.end("Hello");
+  if (request.url === "/api/fish/1" || request.url === "/api/fish/2") {
+    response.statusCode = 200;
+    response.end(request.url === "/api/fish/1" ? "Schrimp" : "Anemonefish");
+  } else {
+    response.statusCode = 404;
+    response.end("Not found");
+  }
 });
