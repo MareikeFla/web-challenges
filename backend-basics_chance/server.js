04cb84120ad83bf5b1643f7b3d1e27a92c544b51
MareikeFla <mwidderich@gmail.com>
2024-02-05 10:34:56 +0100

    initial api routes challenge

--- backend-basics_chance/server.js
+++ backend-basics_chance/server.js
@@ -1,3 +1,11 @@
 import { createServer } from "node:http";
+import Chance from "chance";
+const chance = new Chance();
+const name = chance.name();
+const age = chance.age();
+const profession = chance.profession();
 
-// export const server = …
+export const server = createServer((request, response) => {
+  response.statusCode = 200;
+  response.end(name + " " + age + " " + profession);
+});
